483880e24eb74ff7f6fe6ea8235a7884b9aecfde
Asus-Jame <nokcod@gmail.com>
2018-12-05 18:55:24 +0700

    finish2

--- app/src/main/java/com/example/asus/lab_2/Profile.java
+++ app/src/main/java/com/example/asus/lab_2/Profile.java
@@ -1,11 +1,15 @@
 package com.example.asus.lab_2;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Profile extends AppCompatActivity {
     MediaPlayer mediaPlayer;
+    Button BTNNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +19,14 @@ public class Profile extends AppCompatActivity {
         mediaPlayer = MediaPlayer.create(this,R.raw.c1);
         mediaPlayer.start();
         mediaPlayer.setLooping(true);
+
+        BTNNext = (Button)findViewById(R.id.btnNext);
+        BTNNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Profile.this,Second.class);
+                startActivity(intent);
+            }
+        });
     }
 }
